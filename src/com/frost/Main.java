33a521bbf632f3602d2a47c43c1bc3a29d841b83
frost8111981 <73012443+frost8111981@users.noreply.github.com>
2021-02-19 22:24:09 +0300

    Initial commit

---
+++ src/com/frost/Main.java
@@ -0,0 +1,18 @@
+package com.frost;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+public class Main {
+
+    public final static int  A = 5;
+    public final static int B = 2;
+    public final static int C = A * B;
+
+    public static void main(String[] args) {
+    }
+
+    public  int getValue() {
+        return C;
+    }
+}
